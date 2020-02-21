8804fe5100bb58d35a7a3c206486057d8b4d17f2
sacsnz <EMAIL_ADDRESS>
2020-02-21 02:46:09 +0000

    Update registrations controller

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,10 @@
 // about supported directives.
 //
 //= require rails-ujs
+//= require jquery
+//= require twitter/bootstrap
 //= require activestorage
 //= require turbolinks
-//= require_tree .
+//= require_tree 
+
+
